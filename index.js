41f10bbc1b4bc2beb11cd781cfe7c8fba14cbd1b
Andrew Borzillo <dzillo1@gmail.com>
2026-01-22 08:31:03 -0500

    added app.get

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-
 const routes = require("./routes");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Secure API is running");
+});
 
 app.use("/", routes);
 
