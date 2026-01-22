90f0716d2bb23a601581f03b24eb34f5202f7a23
Andrew Borzillo <dzillo1@gmail.com>
2026-01-22 16:24:06 -0500

    edits path

--- api/index.js
+++ api/index.js
@@ -1,13 +1,12 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const routes = require("./api/routes");
+const routes = require("./routes");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
 app.use(routes);
 
 const PORT = process.env.PORT || 3000;
